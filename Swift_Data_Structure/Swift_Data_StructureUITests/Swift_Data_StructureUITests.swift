89e8faceca43d6c04d5ffc7667875c5e5649a33a
Dima <dima.chumakovwork@gmail.com>
2021-08-17 12:58:31 +0300

    initial commit

---
+++ Swift_Data_Structure/Swift_Data_StructureUITests/Swift_Data_StructureUITests.swift
@@ -0,0 +1,12 @@
+//
+//  Swift_Data_StructureUITests.swift
+//  Swift_Data_StructureUITests
+//
+//  Created by Dmytro Chumakov on 17.08.2021.
+//
+
+import XCTest
+
+class Swift_Data_StructureUITests: XCTestCase {
+
+}
